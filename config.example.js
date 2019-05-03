b57e790cd5f3114da9395fcf95efe816d291782f
Androz2091 <androz2091@gmail.com>
2019-05-03 19:00:08 +0200

    Renaming config.js.example to config.example.js

---
+++ config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+    token:"xxxxxxxxxx",
+    defaultLanguage:"xxxxxxxxxx",
+    embed:{
+        color:"xxxxxxxxxx",
+        footer:"xxxxxxxxxx"
+    },
+    owner:"xxxxxxxxxx",
+    prefix:"xxxxxxxxxx"
+}
