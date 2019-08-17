7f8f157263f1881ea24486612a1e2611657750e5
Androz <androz2091@gmail.com>
2019-08-17 12:51:13 +0200

    fix(guildDelete): update logs

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -11,6 +11,5 @@ module.exports = class {
         // Sends log embed in the logs channel
         var embed = new Discord.RichEmbed().setAuthor(guild.name, guild.iconURL).setColor("#B22222").setDescription("Quelqu'un m'a expulsé de **"+guild.name+"** avec **"+guild.members.filter((m) => !m.user.bot).size+"** membres (et "+guild.members.filter((m) => m.user.bot).size+" bots)");
         this.client.channels.get(this.client.config.supportGuild.serversLogs).send(embed);
-        this.client.channels.get("569976810234707982").send(embed);
     }
 };
