74ca9d3d36c3f752fbbcdc9ad41c840ed9677b0b
Androz <androz2091@gmail.com>
2019-08-17 12:51:41 +0200

    fix(guildCreate): update logs

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -11,6 +11,5 @@ module.exports = class {
         // Sends log embed in the logs channel
         var embed = new Discord.RichEmbed().setAuthor(guild.name, guild.iconURL).setColor("#32CD32").setDescription("J'ai rejoint **"+guild.name+"**, avec **"+guild.members.filter((m) => !m.user.bot).size+"** membres (et "+guild.members.filter((m) => m.user.bot).size+" bots)");
         this.client.channels.get(this.client.config.supportGuild.serversLogs).send(embed);
-        this.client.channels.get("569976810234707982").send(embed);
     }
 };
