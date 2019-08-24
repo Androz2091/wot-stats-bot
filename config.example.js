789e36b43937da7133e39333b87034dcd1da84a1
Androz2091 <androz2091@gmail.com>
2019-08-24 12:17:29 +0200

    Update config

--- config.example.js
+++ config.example.js
@@ -1,24 +1,27 @@
 module.exports = {
-    token:"Your DiscordBot Token", // Your discord bot token (https://discordapp.com/developers/applications/)
-    defaultLanguage:"en", // The default language for new guilds and in direct messages (en or fr)
-    embed:{
-        color:"#FF0000", // The color for the rich embeds
-        footer:"World of Tanks Stats | Open source" // The footer for the rich embeds
+    token: "token",
+    defaultLanguage: "en",
+    embed: {
+        color: "#FF0000",
+        footer: "World of Tanks Stats | Open source"
     },
-    admins:["adminID","adminID"], // An array of bot owners discord ID
-    prefix:"w!", // The default prefix for new guilds
-    wargaming:"xxxxxxxxxx", // Your wargaming API key (https://eu.wargaming.net/id/signin)
+    admins: [ "array of admins ID" ],
+    prefix: "w!",
+    wargaming: "wargaming api key",
     supportGuild: {
-        ID:"xxxxxxxxxx", // The ID of your support guild
-        suggestions:"xxxxxxxxxx", // The channel ID for suggestions
-        serversLogs:"xxxxxxxxxx", // The channel ID for servers logs (new guilds)
-        commandsLogs:"xxxxxxxxxx" // The channel ID for commands logs
+        ID: "server id",
+        suggestions: "channel id",
+        serversLogs:[
+            "array of channels id"
+        ],
+        commandsLogs: "channel id"
     },
     emojis:{
-        success:"<:success:575371114453139476>",
-        error:"<:error:575371154999476224>",
-        loading:"<a:loading:575370909087694849>",
-        warn:"<:warn:575370981225398292>",
-        stats:"<:stats:575371695746187314>"
-    }
-};
+        success:    "emoji string",
+        error:      "emoji string",
+        loading:    "emoji string",
+        warn:       "emoji string",
+        stats:      "emoji string"
+    },
+    dbl: "dbl api key"
+}
