9f154b4f265e5daecee2ec46fc08682f961a4d04
Androz2091 <androz2091@gmail.com>
2019-09-04 18:47:03 +0200

    Update clan stats command with new system

--- commands/Stats/clan-stats.js
+++ commands/Stats/clan-stats.js
@@ -28,97 +28,96 @@ class ClanStats extends Command {
 
     async run (message, args, utils) {
         
-        var client = this.client;
+        let client = this.client;
 
-        message.channel.send(message.language.get("PLEASE_WAIT")).then(async (m) => {
+        let m = await message.channel.send(message.language.get("PLEASE_WAIT"));
 
-            var clanData;
+        let clanData;
 
-            if(message.mentions.users.first()){
-                if(utils.usersData[1].wot === "unknow"){
-                    return m.edit(message.language.get("NOT_LINKED_USER", message.mentions.users.first()));
-                } else {
-                    clanData = await client.functions.getClan(utils.usersData[1].wot.account_id, client).catch((err) => {
-                        return m.edit(message.language.get("NO_CLAN_USER", message.mentions.users.first().tag));
-                    });
+        if(message.mentions.users.first()){
+            if(utils.usersData[1].wot === "unknow"){
+                return m.edit(message.language.get("NOT_LINKED_USER", message.mentions.users.first()));
+            } else {
+                let userStats = await client.Wargamer.getPlayerStats({ realm: utils.userData[1].wot.realm, ID: utils.userData[1].wot.ID }, false);
+                if(!userStats.clan_id){
+                    return m.edit(message.language.get("NO_CLAN_USER", message.mentions.users.first().tag));
                 }
-            } else if(args[0]){
-                // Search all clans
-                clanData = await client.functions.searchClan(args.join(" "), client).catch((err) => {
-                    return m.edit(message.language.get("CLAN_NOT_FOUND", args.join(" ")));
-                });
-            } else if(!args[0]) {
-                if(utils.usersData[0].wot === "unknow"){
-                    return m.edit(message.language.get("NOT_LINKED", utils.guildData.prefix));
-                } else {
-                    clanData = await client.functions.getClan(utils.usersData[0].wot.account_id, client).catch((err) => {
-                        return m.edit(message.language.get("NO_CLAN"));
-                    });
-                }
+                clanData = { realm: utils.usersData[1].wot.realm, ID: userStats.clan_id };
             }
-
-            if(clanData.clan_id){
-                // Gets the stats of the clan
-                var clanStats = await client.functions.getClanStats(clanData.clan_id, client);
-                var clanWN8 = await client.functions.getClanWN8(clanData.clan_id, clanStats.tag);
-
-                var embed = new Discord.RichEmbed()
-                    .setColor(clanWN8.color)
-                    .setFooter(utils.embed.footer)
-                    .setAuthor(clanStats.name, clanStats.emblems.x195.portal)
-                    .addField(message.language.get("CLANSTATS_HEADERS")[0], "["+clanStats.name+"](https://eu.wargaming.net/clans/wot/"+clanData.clan_id+"/)\n\n"+message.language.get("CLANSTATS_TITLES")[0], true);
-
-                var ranks = {
-                    "10":"X",
-                    "8":"VIII",
-                    "6":"VI"
+        } else if(args[0]){
+            let realm = client.realms.find((r) => r.name === args[0] || r.aliases.includes(args[0]));
+            if(!realm) return m.edit(message.language.get("LINK_BAD_REALM", args[0]));
+            if(!args[1]) return m.edit(message.language.get("VALID_CLAN"));
+            clanData = await client.Wargamer.findClan({ search: args.slice(1).join(" "), realm: args[0] }).catch((err) => {
+                return m.edit(message.language.get("CLAN_NOT_FOUND", args.slice(1).join(" ")));
+            });
+        } else if(!args[0]){
+            if(utils.usersData[0].wot === "unknow"){
+                return m.edit(message.language.get("NOT_LINKED", utils.guildData.prefix));
+            } else {
+                let userStats = await client.Wargamer.getPlayerStats({ realm: utils.usersData[0].wot.realm, ID: utils.usersData[0].wot.ID }, false);
+                if(!userStats.clan_id){
+                    return m.edit(message.language.get("NO_CLAN_USER", message.mentions.users.first().tag));
                 }
+                clanData = { realm: utils.usersData[0].wot.realm, ID: userStats.clan_id };
+            }
+        }
 
-                for(var rank in ranks){
-                    if ({}.hasOwnProperty.call(ranks, rank)) {
-                        var title = message.language.get("CLANSTATS_RANKS")[ranks[rank]];
-                        var newTitle = ((rank === Object.keys(ranks)[Object.keys(ranks).length - 1]) ? ("\n\n"+message.language.get("CLANSTATS_TITLES")[1]) : "");
-                        embed.addField(title,
-                            message.language.get("CLANSTATS_FIELDS")[0][0]+
-                            ((clanStats.skirmish_statistics["last_time_"+rank] === 0) ? message.language.get("NO_RECENT_BATTLE") : message.language.printDate(new Date(clanStats.skirmish_statistics["last_time_"+rank]*1000)))+"\n"+
-                            message.language.get("CLANSTATS_FIELDS")[0][1]+
-                            (clanStats.skirmish_statistics["total_"+rank]+" ("+clanStats.skirmish_statistics["win_"+rank]+" "+message.language.get("WINS")+")")+"\n"+
-                            message.language.get("CLANSTATS_FIELDS")[0][2]+
-                            (clanStats.skirmish_statistics["total_"+rank+"_in_28d"]+" ("+clanStats.skirmish_statistics["win_"+rank+"_in_28d"]+" "+message.language.get("WINS")+")")+"\n"+
-                            message.language.get("CLANSTATS_FIELDS")[0][3]+
-                            ((clanStats.skirmish_statistics["win_"+rank] === 0) ? "0%" : client.functions.percentage(clanStats.skirmish_statistics["win_"+rank], clanStats.skirmish_statistics["lose_"+rank]))+"\n"+
-                            message.language.get("CLANSTATS_FIELDS")[0][4]+
-                            ((clanStats.skirmish_statistics["win_"+rank+"_in_28d"] === 0) ? "0%" : client.functions.percentage(clanStats.skirmish_statistics["win_"+rank+"_in_28d"], clanStats.skirmish_statistics["total_"+rank+"_in_28d"]-clanStats.skirmish_statistics["win_"+rank+"_in_28d"]))+newTitle
-                        );
-                    }
-                }
+        if(!clanData) return;
+        // Gets the stats of the clan
+        let clanStats = await client.Wargamer.getClanStats({ realm: clanData.realm, ID: clanData.ID });
 
-                embed.addField(message.language.get("CLANSTATS_HEADERS")[1],
-                    message.language.get("CLANSTATS_FIELDS")[1][0]+
-                    (clanStats.stronghold_buildings_level)+"\n"+
-                    message.language.get("CLANSTATS_FIELDS")[1][1]+
-                    (clanStats.stronghold_level)+"\n"+
-                    message.language.get("CLANSTATS_FIELDS")[1][2]+
-                    ((clanStats.battles_for_strongholds_statistics.last_time_10 === 0) ? message.language.get("NO_RECENT_BATTLE") : message.language.printDate(new Date(clanStats.battles_for_strongholds_statistics.last_time_10*1000)))+"\n"+
-                    message.language.get("CLANSTATS_FIELDS")[1][3]+
-                    (clanStats.battles_for_strongholds_statistics.total_10)+"\n"+
-                    message.language.get("CLANSTATS_FIELDS")[1][4]+
-                    (clanStats.battles_for_strongholds_statistics.total_10_in_28d)+"\n"+
-                    message.language.get("CLANSTATS_FIELDS")[1][5]+
-                    ((clanStats.battles_for_strongholds_statistics.win_10 === 0) ? "0%" : client.functions.percentage(clanStats.battles_for_strongholds_statistics.win_10, clanStats.battles_for_strongholds_statistics.lose_10))+"\n"+
-                    message.language.get("CLANSTATS_FIELDS")[1][6]+
-                    ((clanStats.battles_for_strongholds_statistics.win_10_in_28d === 0) ? "0%" : client.functions.percentage(clanStats.battles_for_strongholds_statistics.win_10_in_28d, clanStats.battles_for_strongholds_statistics.total_10_in_28d-clanStats.battles_for_strongholds_statistics.win_10_in_28d))+"\n\n"+message.language.get("CLANSTATS_TITLES")[2]
+        let embed = new Discord.RichEmbed()
+            .setColor(clanStats.wn8.color)
+            .setFooter(utils.embed.footer)
+            .setAuthor(clanStats.name, clanStats.emblems.x195.portal)
+            .addField(message.language.get("CLANSTATS_HEADERS")[0], "["+clanStats.name+"](https://eu.wargaming.net/clans/wot/"+clanData.clan_id+"/)\n\n"+message.language.get("CLANSTATS_TITLES")[0], true);
+
+        let ranks = { "10":"X", "8":"VIII", "6":"VI" }
+
+        for(let rank in ranks){
+            if ({}.hasOwnProperty.call(ranks, rank)) {
+                let title = message.language.get("CLANSTATS_RANKS")[ranks[rank]];
+                let newTitle = ((rank === Object.keys(ranks)[Object.keys(ranks).length - 1]) ? ("\n\n"+message.language.get("CLANSTATS_TITLES")[1]) : "");
+                embed.addField(title,
+                    message.language.get("CLANSTATS_FIELDS")[0][0]+
+                    ((clanStats.skirmish_statistics["last_time_"+rank] === 0) ? message.language.get("NO_RECENT_BATTLE") : message.language.printDate(new Date(clanStats.skirmish_statistics["last_time_"+rank]*1000)))+"\n"+
+                    message.language.get("CLANSTATS_FIELDS")[0][1]+
+                    (clanStats.skirmish_statistics["total_"+rank]+" ("+clanStats.skirmish_statistics["win_"+rank]+" "+message.language.get("WINS")+")")+"\n"+
+                    message.language.get("CLANSTATS_FIELDS")[0][2]+
+                    (clanStats.skirmish_statistics["total_"+rank+"_in_28d"]+" ("+clanStats.skirmish_statistics["win_"+rank+"_in_28d"]+" "+message.language.get("WINS")+")")+"\n"+
+                    message.language.get("CLANSTATS_FIELDS")[0][3]+
+                    ((clanStats.skirmish_statistics["win_"+rank] === 0) ? "0%" : client.functions.percentage(clanStats.skirmish_statistics["win_"+rank], clanStats.skirmish_statistics["lose_"+rank]))+"\n"+
+                    message.language.get("CLANSTATS_FIELDS")[0][4]+
+                    ((clanStats.skirmish_statistics["win_"+rank+"_in_28d"] === 0) ? "0%" : client.functions.percentage(clanStats.skirmish_statistics["win_"+rank+"_in_28d"], clanStats.skirmish_statistics["total_"+rank+"_in_28d"]-clanStats.skirmish_statistics["win_"+rank+"_in_28d"]))+newTitle
                 );
-
-                embed.addField(clanWN8.now, "\u200B");
-
-                if(clanStats.motto){
-                    embed.setDescription(clanStats.motto);
-                }
-
-                m.edit(message.language.get("CLANSTATS_SUCCESS", clanStats.name), embed);
             }
-        });
+        }
+
+        embed.addField(message.language.get("CLANSTATS_HEADERS")[1],
+            message.language.get("CLANSTATS_FIELDS")[1][0]+
+            (clanStats.stronghold_buildings_level)+"\n"+
+            message.language.get("CLANSTATS_FIELDS")[1][1]+
+            (clanStats.stronghold_level)+"\n"+
+            message.language.get("CLANSTATS_FIELDS")[1][2]+
+            ((clanStats.battles_for_strongholds_statistics.last_time_10 === 0) ? message.language.get("NO_RECENT_BATTLE") : message.language.printDate(new Date(clanStats.battles_for_strongholds_statistics.last_time_10*1000)))+"\n"+
+            message.language.get("CLANSTATS_FIELDS")[1][3]+
+            (clanStats.battles_for_strongholds_statistics.total_10)+"\n"+
+            message.language.get("CLANSTATS_FIELDS")[1][4]+
+            (clanStats.battles_for_strongholds_statistics.total_10_in_28d)+"\n"+
+            message.language.get("CLANSTATS_FIELDS")[1][5]+
+            ((clanStats.battles_for_strongholds_statistics.win_10 === 0) ? "0%" : client.functions.percentage(clanStats.battles_for_strongholds_statistics.win_10, clanStats.battles_for_strongholds_statistics.lose_10))+"\n"+
+            message.language.get("CLANSTATS_FIELDS")[1][6]+
+            ((clanStats.battles_for_strongholds_statistics.win_10_in_28d === 0) ? "0%" : client.functions.percentage(clanStats.battles_for_strongholds_statistics.win_10_in_28d, clanStats.battles_for_strongholds_statistics.total_10_in_28d-clanStats.battles_for_strongholds_statistics.win_10_in_28d))+"\n\n"+message.language.get("CLANSTATS_TITLES")[2]
+        );
+
+        embed.addField(clanStats.wn8.now, "\u200B");
+
+        if(clanStats.motto){
+            embed.setDescription(clanStats.motto);
+        }
+
+        m.edit(message.language.get("CLANSTATS_SUCCESS", clanStats.name), embed);
     }
 
 };
