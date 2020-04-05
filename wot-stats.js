2449c9d24c9d438108a8b06923982ce96462c768
Androz2091 <androz2091@gmail.com>
2020-04-05 09:45:14 +0200

    Update DBL and sharding

--- wot-stats.js
+++ wot-stats.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require("discord.js");
 const manager = new ShardingManager("./index.js", { token: require("./config").token });
 
-manager.spawn(9);
+manager.spawn(2);
 manager.on("launch", (shard) => console.log("Shard #"+shard.id+" launched!"));
