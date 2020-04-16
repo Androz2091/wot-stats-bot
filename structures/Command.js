7d0730f94e4abe2969ade56892eb12d03b4f805b
Androz2091 <androz2091@gmail.com>
2020-04-16 20:30:29 +0200

    fix: remove useless prop

--- structures/Command.js
+++ structures/Command.js
@@ -2,9 +2,6 @@ module.exports = class Command {
     constructor(client, {
         name = null,
         enabled = true,
-        description = (language) => language.get("NO_DESCRIPTION_PROVIDED"),
-        usage = (language) => language.get("NO_USAGE_PROVIDED"),
-        examples = (language) => language.get("NO_EXAMPLES_PROVIDED"),
         aliases = new Array(),
         clientPermissions = new Array(),
         permLevel = "Mega",
@@ -15,6 +12,6 @@ module.exports = class Command {
         let category = dirname ? dirname.split("/")[dirname.split("/").length-1] : "Other";
         this.client = client;
         this.conf = { enabled, aliases, permLevel, clientPermissions, cooldown};
-        this.help = { name, description, usage, examples, category };
+        this.help = { name, category };
     }
 };
