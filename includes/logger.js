80a80a8a986395b98456bc4ddb754a982d8e6255
Androz2091 <androz2091@gmail.com>
2020-04-17 12:23:46 +0200

    :art: Prettify logger

--- includes/logger.js
+++ includes/logger.js
@@ -1,50 +1,48 @@
-/*
-Logger class for easy and aesthetically pleasing console logging
-*/
-const { bgBlue, bdRed, black, green } = require("chalk");
+const { bgBlue, black, green } = require("chalk");
 const moment = require("moment");
 
 module.exports = class Logger {
-  static log (content, type = "log") {
-    const timestamp = "["+moment().format("YYYY-MM-DD HH:mm:ss")+"]:";
-    switch (type) {
-      // Check the message type and then print him in the console
-      case "log": {
-        return console.log(timestamp + " " + bgBlue(type.toUpperCase()) + " " + content);
-      }
-      case "warn": {
-        return console.log(timestamp + " " + black.bgYellow(type.toUpperCase()) + " " + content);
-      }
-      case "error": {
-        return console.log(timestamp + " " +  black.bgRed(type.toUpperCase()) + " " +  content);
-      }
-      case "debug": {
-        return console.log(timestamp + " " + green(type.toUpperCase())+ " " + content);
-      }
-      case "cmd": {
-        return console.log(timestamp + " " + black.bgWhite(type.toUpperCase()) + " " + content);
-      }
-      case "ready": {
-        return console.log(timestamp + " " + black.bgGreen(type.toUpperCase()) + " " + content);
-      } 
-      default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
+    static log (content, type = "log") {
+
+        const timestamp = "["+moment().format("YYYY-MM-DD HH:mm:ss")+"]:";
+        switch (type) {
+            // Check the message type and then print it in the console
+            case "log": {
+                return console.log(timestamp + " " + bgBlue(type.toUpperCase()) + " " + content);
+            }
+            case "warn": {
+                return console.log(timestamp + " " + black.bgYellow(type.toUpperCase()) + " " + content);
+            }
+            case "error": {
+                return console.log(timestamp + " " +  black.bgRed(type.toUpperCase()) + " " +  content);
+            }
+            case "debug": {
+                return console.log(timestamp + " " + green(type.toUpperCase())+ " " + content);
+            }
+            case "cmd": {
+                return console.log(timestamp + " " + black.bgWhite(type.toUpperCase()) + " " + content);
+            }
+            case "ready": {
+                return console.log(timestamp + " " + black.bgGreen(type.toUpperCase()) + " " + content);
+            } 
+            default: throw new TypeError("Logger type must be either warn, debug, log, ready, cmd or error.");
+        }
     }
-  }
   
-  static error (content) {
-    this.log(content, "error");
-  }
-  
-  static warn (content) {
-    this.log(content, "warn");
-  }
-  
-  static debug (content) {
-    this.log(content, "debug");
-  }
-  
-  static cmd (content) {
-    this.log(content, "cmd");
-  }
+    static error (content) {
+        this.log(content, "error");
+    }
+    
+    static warn (content) {
+        this.log(content, "warn");
+    }
+    
+    static debug (content) {
+        this.log(content, "debug");
+    }
+    
+    static cmd (content) {
+        this.log(content, "cmd");
+    }
 
 };
